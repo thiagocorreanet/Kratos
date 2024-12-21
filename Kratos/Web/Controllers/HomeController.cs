6318b21c17b5dced1c56c4677d5ca23e05d93e1c
Thiago <thiagogcbotelho@hotmail.com>
2024-12-20 21:50:45 -0300

    Refactor controllers and update views and styles
    Refactored HomeController and removed unnecessary using directives. Deleted ErrorViewModel and Error.cshtml, indicating error handling changes. Reorganized launchSettings.json for better readability. Updated Index.cshtml and Default.cshtml for content and navigation changes. Enhanced _Layout.cshtml for cache busting and added DataTables libraries. Removed Web.Models import from _ViewImports.cshtml. Added ProjectsController and GetById view for project management. Updated Web.csproj with new package reference. Introduced new CSS styles for tables and DataTables in custom.css.

--- Kratos/Web/Controllers/HomeController.cs
+++ Kratos/Web/Controllers/HomeController.cs
@@ -1,32 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-using Web.Models;
 
-namespace Web.Controllers
+namespace Web.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
+        _logger = logger;
+    }
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    public IActionResult Privacy()
+    {
+        return View();
     }
 }
---
+++ Kratos/Web/Controllers/ProjectsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        [HttpGet("lista-de-projetos")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("consultar-projeto")]
+        public IActionResult GetById(int id)
+        {
+            return View();
+        }
+    }
+}

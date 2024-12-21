0158e1b1a1d4d1ad400a64d2cdc3274aacd598b4
Thiago <thiagogcbotelho@hotmail.com>
2024-12-20 23:07:47 -0300

    Remove UnitTest project; update Web project references
    The UnitTest project has been removed from the solution file (Kratos.sln), including its project definition and build configurations.
    
    In ProjectsController.cs, commented-out code for a private _mediator field has been added.
    
    In Web.csproj, the Microsoft.VisualStudio.Web.CodeGeneration.Design package reference has been re-added. New package references for Microsoft.EntityFrameworkCore.Design, Refit, and Refit.HttpClientFactory have been added. Additionally, project references to Application, Core, and Infrastructure projects have been included.

--- Kratos/Web/Controllers/ProjectsController.cs
+++ Kratos/Web/Controllers/ProjectsController.cs
@@ -4,6 +4,8 @@ namespace Web.Controllers
 {
     public class ProjectsController : Controller
     {
+        //private IMediator _mediator;
+
         [HttpGet("lista-de-projetos")]
         public IActionResult Index()
         {
